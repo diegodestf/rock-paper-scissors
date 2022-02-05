7e0445c70f83ba9959705c8d9d01c4a5a5d37e89
diegodestf <diegodesteffano@gmail.com>
2022-02-05 10:06:08 -0300

    Refactored a few functions so they are better suited for the UI

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const actionMessage = document.getElementById("action-message");
+const playerScorePara = document.getElementById("player-score");
+const computerScorePara = document.getElementById("computer-score");
+const result = document.querySelector(".result > p");
+
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay() {
     
     const rndInt = Math.floor(Math.random() * 3) + 1;
     
     if (rndInt === 1){
-        return "Rock";
+        return "r";
     } else if (rndInt === 2) {
-        return "Paper";
+        return "p";
     } else {
-        return "Scissors";
+        return "s";
     }
     
 }
-
-let playerScore = 0;
-let computerScore = 0;
 
 function gameOver() {
 
@@ -22,83 +31,62 @@ function gameOver() {
 
 function endGameMessage(){
 
-    return playerScore > computerScore ? (scoreHeading.textContent = `The final score is Player: ${playerScore} to Computer: ${computerScore}. Player Wins!`) : 
-    (scoreHeading.textContent =`The final score is Player: ${playerScore} to Computer: ${computerScore}. Computer Wins!`);
+    return playerScore > computerScore ? (result.textContent = `The final score is Player: ${playerScore} to Computer: ${computerScore}. Player Wins!`) : 
+    (result.textContent =`The final score is Player: ${playerScore} to Computer: ${computerScore}. Computer Wins!`);
 
 
 }
 
+function win(){
+    playerScore++;
+    playerScorePara.textContent = playerScore;
+    computerScorePara.textContent = computerScore;
+}
 
-function singleRound(e) {
+function lose() {
+    computerScore++;
+    playerScorePara.textContent = playerScore;
+    computerScorePara.textContent = computerScore;
+
+}
+
+function draw(){
+    return;
+}
+
+function game(playerSelection){
+    const computerSelection = computerPlay();
+    switch (playerSelection + computerSelection) {
+        case 'pr':
+        case 'sp':
+        case 'rs':
+            win();
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose()
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw();
+            break;
+
+    }
     if (gameOver()) {
         endGameMessage();
-        alert("Game over. Press F5 to play again");
-        return;
+        alert("Press F5 to play again.")
     }
-    
-    const computerSelection = computerPlay();
-    const playerSelection = e.target.id.toLowerCase();
-    updateScore(roundWinner(playerSelection, computerSelection));
-}
+}  
 
-function roundWinner(playerSelection, computerSelection){
-    switch (playerSelection) {
-        case 'rock':
-            if (computerSelection === "Rock") {
-                actionMessage.textContent = "It\'s a tie!"
-            } else if (computerSelection === "Paper") {
-                actionMessage.textContent = "You lose! Paper beats Rock";
-            } else {
-                actionMessage.textContent = "You win! Rock beats Scissors";
-            }
-            break;
-            
-            case 'paper':
-                if (computerSelection === "Rock") {
-                    actionMessage.textContent = "You win! Paper beats Rock";
-                } else if (computerSelection === "Paper") {
-                    actionMessage.textContent = "It\'s a tie!";
-                } else {
-                    actionMessage.textContent = "You lose! Scissors beats Paper!";
-                }
-                break;
-                
-                case 'scissors':
-                    if (computerSelection === "Rock") {
-                        actionMessage.textContent = "You lose! Rock beats Scissors";
-                    } else if (computerSelection === "Paper") {
-                        actionMessage.textContent = "You win! Scissors beats Paper";
-                    } else {
-                        actionMessage.textContent = "It\'s a tie!";
-                    }
-                break;
 
-    }
-}
-    
-function updateScore(winner) {
-    if (scoreHeading.textContent.startsWith("It")) {
-        return;
-    } else if (actionMessage.textContent.startsWith("You win")) {
-        playerScore++;
-    } else if (actionMessage.textContent.startsWith("You lose")) {
-        computerScore++;
-    }
-    playerScorePara.textContent = `Player:  ${playerScore}`;
-    computerScorePara.textContent = `Computer: ${computerScore}`;
-
-    if (gameOver()){
-        endGameMessage();
-    }
-}       
-
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-const actionMessage = document.querySelector("#action-message");
-const playerScorePara = document.querySelector("#player-label");
-const computerScorePara = document.querySelector("#computer-label");
-
-rock.addEventListener("click", singleRound);
-paper.addEventListener("click", singleRound);
-scissors.addEventListener("click", singleRound);
+rock.addEventListener("click", function() {
+    game("r")
+});
+paper.addEventListener("click", function() {
+    game("p")
+});
+scissors.addEventListener("click", function() {
+    game("s")
+});
